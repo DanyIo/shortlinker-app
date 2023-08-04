9ee6d946e15eefe41d1cdd596dbba4c40c8fa256
Halvaya <danikbodnar@gmail.com>
2023-08-04 23:43:08 +0200

    automated mail verification

---
+++ verifyEmailIdentity.mjs
@@ -0,0 +1,43 @@
+import {
+  SESClient,
+  VerifyEmailIdentityCommand,
+  GetIdentityVerificationAttributesCommand,
+} from "@aws-sdk/client-ses";
+
+const clientConfig = {
+  region: process.env.REGION,
+};
+
+const client = new SESClient(clientConfig);
+
+const email = process.env.VERIFIED_EMAIL ?? "no@gmail.com";
+
+
+const verifyEmailIdentity = async () => {
+  try {
+    const response = await client.send(
+      new GetIdentityVerificationAttributesCommand({
+        Identities: [email],
+      })
+    );
+
+    const isEmailVerified = response?.VerificationAttributes?.[email];
+    if (isEmailVerified) {
+      console.log(`${email} is already verified.`);
+      return;
+    }
+
+    await client.send(
+      new VerifyEmailIdentityCommand({
+        EmailAddress: email,
+      })
+    );
+
+    console.log(`${email} was successfully verified.`);
+    console.log("Check your email for the verification request.");
+  } catch (error) {
+    console.error("Error verifying email:", error.message);
+  }
+};
+
+verifyEmailIdentity();
